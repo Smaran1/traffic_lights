c7640889fbf8b47aafea37df12228873705b5512
Smaran <smaran@uxcam.com>
2023-05-30 08:05:19 +0545

    editing

--- duration.py
+++ duration.py
@@ -4,17 +4,18 @@ def traffic_duration(looping_times,range_for_green, range_for_red):
         green_light = reversed(range(1, range_for_green))
         red_light = reversed(range(1, range_for_red))
 
-        print("GO")
+        
         for g in green_light:
             print(g)
             if g == 1:
                 print("STOP")
+           
 
     
         for r in red_light:
             print(r)
             if r == 1:
-                print("STOP")
+                print("GO")
 
 
 traffic_duration(2,61,121)
